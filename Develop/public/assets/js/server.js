5a010aa939703fd7ba2b6b26eeceea17541003b7
Chris Ferguson <cfergus7@gmail.com>
2020-06-22 23:08:11 -0500

    i think it works

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,55 +2,91 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const fs = require("fs")
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
 // Routes
 // =============================================================
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"))
+app.get("/api/notes", (err, res) => {
+    try {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    res.json(notesData);
 });
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"))
+app.post("/api/notes", (req, res) => {
+    try {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        console.log(notesData);
+        notesData = JSON.parse(notesData);
+        req.body.id = notesData.length;
+        notesData.push(req.body);
+        notesData = JSON.stringify(notesData);
+        fs.writeFile("./db/db.json", notesData, "utf8", function (err) {
+            if (err) throw err;
+        });
+        res.json(JSON.parse(notesData));
+
+    } catch (err) {
+        throw err;
+        console.error(err);
+    }
 });
 
-app.get("/api/notes", function(req, res) {
-    var result = {
-        "data": note
-    };
-    res.json(result);
+app.delete("/api/notes/:id", (req, res) => {
+    try {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+        notesData = notesData.filter(function (note) {
+            return note.id != req.params.id;
+        });
+        notesData = JSON.stringify(notesData);
+        fs.writeFile("./db/db.json", notesData, "utf8", function (err) {
+            if (err) throw err;
+        });
+        res.send(JSON.parse(notesData));
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
 });
 
-app.post("/api/notes", function(req, res) {
-    var result = {
-        "data": note
-    };
-    res.json(result);
-});
-// Look into this but on the right track I think
-app.delete("/api/notes/:id", function(req, res) {
-    var result = {
-        "data": note
-    };
-    res.json(result);
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/api/notes", (req, res) => {
+    return res.sendFile(path.json(__dirname, "./db/db.json"));
+});
 
-
-
-
+function (app) {
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+}
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
